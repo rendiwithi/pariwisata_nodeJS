d431ef8244be807045fc8d4116c499532fc45d9f
rendiwithi <rendidwih2@gmail.com>
2021-12-24 22:10:29 +0700

    change database

--- destinasi_batu (1).sql
+++ destinasi_batu (1).sql
@@ -44,9 +44,7 @@ INSERT INTO `destinasi` (`id_des`, `nama`, `deskripsi`, `image`, `latitude`, `lo
 (5, 'Air Terjun Coban Rondo', ' Air terjun yang terletak di Kecamatan Pujon, Kota Batu,                 Kabupaten Malang, Jawa Timur.Air terjun ini mudah dijangkau oleh kendaraan umum.', '1640169206960.jpg', -7.8849715, 112.4772569),
 (6, 'Alun Alun Kota Batu', 'Sebagai alun-alun daerah Batu, maka tempat ini tak pernah lepas dari keramaian ', '1640169303426.jpg', 19.12092, 14),
 (7, 'Taman Hutan Kota Batu', 'Terletak di jalan Sultan Agung, sebelah barat Stadion Brantas Kota Batu dengan lokasi yang strategis,', '1640183786272.jpg', -7.8787967, 112.522903),
-(8, 'Gunung Panderman', ' Gunung ini memiliki puncak ketinggian 2045 meter di ataspermukaan laut.', '1640183850622.jpg', -7.9041665, 112.4879119),
-(9, 'Rendi Dwi', 'ranu manduro terleatak di mojokerto ', '1640330543959.jpg', 19.12092, 12.84),
-(10, 'iksan arifki', 'air terjun tersebut terletak di kecamatan ...', '1640330648599.jpg', 19.12092, 14);
+(8, 'Gunung Panderman', ' Gunung ini memiliki puncak ketinggian 2045 meter di ataspermukaan laut.', '1640183850622.jpg', -7.9041665, 112.4879119);
 
 --
 -- Indexes for dumped tables
@@ -66,7 +64,7 @@ ALTER TABLE `destinasi`
 -- AUTO_INCREMENT for table `destinasi`
 --
 ALTER TABLE `destinasi`
-  MODIFY `id_des` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id_des` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
